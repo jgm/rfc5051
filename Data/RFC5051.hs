bfa2ca9b3056fe107504d342242839b44e56f339
John MacFarlane <fiddlosopher@gmail.com>
2013-04-28 15:55:41 -0700

    Working rough version of decomposition and comparison.

--- Data/RFC5051.hs
+++ Data/RFC5051.hs
@@ -1,5 +1,20 @@
-module Data.RFC5051 (decompositionMap)
+module Data.RFC5051 (compareUnicode)
 where
-import Data.RFC5051.UnicodeData
+import Data.RFC5051.UnicodeData (decompositionMap)
+import qualified Data.Map as M
+import Data.Char (ord, toTitle)
 
+compareUnicode :: String -> String -> Ordering
+compareUnicode x y = compare (canonicalize x) (canonicalize y)
 
+canonicalize :: String -> [[Int]]
+canonicalize = map (decompose . ord . toTitle)
+
+decompose :: Int -> [Int]
+decompose c =
+  case decompose' c of
+         Nothing -> [c]
+         Just xs -> concatMap decompose xs
+
+decompose' :: Int -> Maybe [Int]
+decompose' c = M.lookup c decompositionMap
--- MkUnicodeData.hs
+++ MkUnicodeData.hs
@@ -1,6 +1,6 @@
 module Main
 where
-import Data.Char (isDigit)
+import Data.Char (isHexDigit)
 import Data.RFC5051.Types
 import qualified Data.ByteString.Char8 as B8
 
@@ -9,13 +9,14 @@ main = B8.readFile "UnicodeData.txt" >>= rawDataToModule
 rawDataToModule :: B8.ByteString -> IO ()
 rawDataToModule bs = do
   let lns = map (B8.split ';') $ take 0x2000 $ B8.lines bs
-  let pairs = [ (toInt x, map toInt $ filter (B8.all isDigit) $ B8.words y) |
+  let pairs = [ (toInt x, map toInt $ filter (B8.all isHexDigit) $ B8.words y) |
                     (x:_:_:_:_:y:_) <- lns, not (B8.null y) ]
   putStrLn "module Data.RFC5051.UnicodeData (decompositionMap) where"
   putStrLn "import Data.RFC5051.Types"
+  putStrLn "import qualified Data.Map as M"
   putStrLn ""
-  putStrLn "decompositionMap :: [(Int, [Int])]"
-  putStrLn $ "decompositionMap = " ++ show pairs
+  putStrLn "decompositionMap :: M.Map Int [Int]"
+  putStrLn $ "decompositionMap = M.fromList " ++ show pairs
 
 toInt :: B8.ByteString -> Int
 toInt xs = read $ '0':'x': B8.unpack xs
